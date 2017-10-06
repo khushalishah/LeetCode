2ae2dd65b210bdf16e96d97249937844b016d9d9
khushali <shahkhushi.17@gmail.com>
2017-10-06 15:28:58 -0700

    ArrayPartition solved

---
+++ src/ArrayPartition.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ArrayPartition {
+	public static void main(String args[]) {
+		
+	}
+	
+	public int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++) {
+        	int n1 = nums[i];
+        	int n2 = nums[++i];
+        	
+        	if(n1<=n2) {
+        		sum += n1;
+        	}else {
+        		sum += n2;
+        	}
+        }
+        return sum;
+    }
+}
