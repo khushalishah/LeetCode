99e5490a0e84eb5fc90ec647edd1848ea8754c8e
khushali <shahkhushi.17@gmail.com>
2017-10-10 23:50:40 -0700

    BaseBallGame committed

---
+++ src/BaseBallGame.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class BaseBallGame {
+
+	public static void main(String args[]) {
+		BaseBallGame bg = new BaseBallGame();
+		System.out.println(bg.calPoints(new String[] {"5","2","C","D","+"}));
+	}
+	
+	public int calPoints(String[] ops) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(String element:ops) {
+        	if(element.equals("C")) {
+        		sum -= stack.pop();
+        	}else if(element.equals("D")) {
+        		int no = 2*stack.peek();
+        		sum += no;
+        		stack.push(no);
+        	}else if(element.equals("+")) {
+        		int no1 = stack.pop();
+        		int no2 = stack.peek();
+        		int total = no1 + no2;
+        		sum += total;
+        		stack.push(no1);
+        		stack.push(total);
+        	}else {
+        		int no = Integer.parseInt(element);
+        		stack.push(no);
+        		sum += no;
+        	}
+        }
+        return sum;
+    }
+	
+}
