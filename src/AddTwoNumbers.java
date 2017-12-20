75bf797ac2308a1f0b41c420bdd3e871c1b951f9
khushali <shahkhushi.17@gmail.com>
2017-12-20 11:37:13 -0800

    Problem 2

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -25,45 +25,6 @@ public class AddTwoNumbers {
 
 	}
 
-	/*public ListNode addNumbers(ListNode l1, ListNode l2) {
-		if(l1 == null) {
-			if(l2 != null) {
-				l2.val += carry;
-				if(l2.val>9) {
-					carry = 1;
-					l2.val = l2.val % 10;
-					if(l2.next == null)
-						l2.next = new ListNode(1);
-				}
-			}
-			return l2;
-		}
-		if(l2 == null) {
-			if(l1 != null) {
-				l1.val += 1;
-				if(l1.val>9) {
-					carry = 1;
-					l1.val = l1.val % 10;
-					if(l1.next == null)
-						l1.next = new ListNode(1);
-				}
-			}
-			return l1;
-		}
-
-		l1.val += l2.val+carry;
-		carry = 0;
-		if(l1.val>9) {
-			carry = 1;
-			l1.val = l1.val % 10;
-			if(l1.next == null && l2.next == null) {
-				l1.next = new ListNode(0);
-			}
-		}
-		l1.next = addNumbers(l1.next, l2.next);
-		return l1;
-	}*/
-
 	public ListNode addNumbers(ListNode l1, ListNode l2) {
 		return addNumbers(l1,l2,0);
 	}
